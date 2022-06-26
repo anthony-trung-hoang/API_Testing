6afb2f692d7cfc7703c8abd03b776cfe3e4e9688
nAts2002 <92941258+nAts2002@users.noreply.github.com>
2022-06-26 23:25:05 +0700

    Minh

--- src/GetDetailAuctionsTest.java
+++ src/GetDetailAuctionsTest.java
@@ -5,16 +5,16 @@ import org.junit.jupiter.api.Test;
 class GetDetailAuctionsTest {
 
     @Test
-    @DisplayName("When Auction_ID is right, response code should be 1000 and message should be OK")
-    public void AuctionIdIs1(){
+    @DisplayName("Unit Test 1: When Auction_ID is right, response code should be 1000 and message should be OK")
+    public void UnitTest1(){
         GetDetailAuctions getDetailAuctions = new GetDetailAuctions();
         getDetailAuctions.Test09(1, null);
         Assertions.assertEquals(1000, getDetailAuctions.getCode());
         Assertions.assertEquals("OK",getDetailAuctions.getMessage());
     }
     @Test
-    @DisplayName("When Auction_ID is 0, HTTP response code should be 500 ")
-    public void AuctionIdIs0(){
+    @DisplayName("Unit Test 2: When Auction_ID is 0, HTTP response code should be 500 ")
+    public void UnitTest2(){
         GetDetailAuctions getDetailAuctions = new GetDetailAuctions();
         getDetailAuctions.Test09(0, null);
         Assertions.assertEquals(500, getDetailAuctions.getHttpResponseCode());
