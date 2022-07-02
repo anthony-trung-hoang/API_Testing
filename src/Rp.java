45b01322ad2068e69eb3ca04fc50c51dd57bb5fa
Hoàng Quốc Trung <hoangquoctrung02@hotmail.com>
2022-07-02 16:04:16 +0700

    Trung them mot so attributes va class dung cho test login

--- src/Rp.java
+++ src/Rp.java
@@ -1,6 +1,15 @@
 // Response Object
+
+class User {
+	// For Test Login
+	String name, role, email, address, avatar, phone;
+}
+
 class Data{
     String access_token;
+    String token_type;
+    String expires_in;
+    User user = new User();
 }
 public class Rp {
     public int code;
