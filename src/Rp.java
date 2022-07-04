b2caed4348cb3b062114fed5a787510b277a4223
Hoàng Quốc Trung <hoangquoctrung02@hotmail.com>
2022-07-04 21:36:05 +0700

    Trung lam xong test 07, Trung them class trong Rp.java

--- src/Rp.java
+++ src/Rp.java
@@ -1,3 +1,5 @@
+import java.lang.reflect.Array;
+
 // Response Object
 
 class User {
@@ -7,11 +9,28 @@ class User {
 
 class Data{
     String access_token;
-
     String token_type;
     String expires_in;
     User user = new User();
+    Auction[] auctions;
+    String total;
 }
+
+class Category{
+	String name;
+	String image;
+	String type;
+}
+
+class Auction{
+	String auction_id;
+    String selling_user_id;
+    String title;
+    String start_date, end_date;
+    String statusId, status;
+    Category catergory = new Category();
+}
+
 public class Rp {
     public int code;
     public String message;
