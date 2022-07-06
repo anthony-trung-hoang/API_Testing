a35113d5bb5c87e26b7f037d070c97f8ff71a978
phuongdd204680 <103870562+phuongdd204680@users.noreply.github.com>
2022-07-06 22:33:19 +0700

    Phuong them

--- src/SignUpTests.java
+++ src/SignUpTests.java
@@ -9,7 +9,9 @@ public class SignUpTests {
     @Test
     public void unitTest1(){
 
+        System.out.println("If all is not null, code should be 1000 and message should be OK");
         System.out.println("Testing unit1...");
+
         for(int i = 1; i <= 10; i++){
             String randomMail = email.getStringWithFixedLength(10) + "@gmail.com";
             String randomPass = email.getStringWithFixedLength(10);
@@ -18,12 +20,14 @@ public class SignUpTests {
             Assertions.assertEquals(1000, signUp.getCode());
             Assertions.assertEquals("OK", signUp.getMessage());
         }
-        System.out.println("Unit 1: Satisfied");
+        System.out.println("Unit 1: Satisfied!");
     }
     @Test
     public void unitTest2(){
 
+        System.out.println("If email > 255, code should be 1001 and message should be name &phone: &address: &email: 7001&password: &re_pass:  &avatar: ");
         System.out.println("Testing unit2...");
+
         for(int i = 1; i <= 10; i++){
             String randomMail = email.getStringWithFixedLength(260) + "@gmail.com";
             String randomPass = email.getStringWithFixedLength(10);
@@ -32,12 +36,14 @@ public class SignUpTests {
             Assertions.assertEquals(1001, signUp.getCode());
             Assertions.assertEquals("name: &phone: &address: &email: 7001&password: &re_pass:  &avatar: ", signUp.getMessage());
         }
-        System.out.println("Unit 2: Satisfied");
+        System.out.println("Unit 2: Satisfied!");
     }
     @Test
     public void unitTest3() {
 
+        System.out.println("If password > 255, code should be 1001 and message should be name: &phone: &address: &email: &password: 7001&re_pass: 7001 &avatar: ");
         System.out.println("Testing unit3...");
+
         for (int i = 1; i <= 10; i++) {
             String randomMail = email.getStringWithFixedLength(10) + "@gmail.com";
             String randomPass = email.getStringWithFixedLength(260);
@@ -46,7 +52,7 @@ public class SignUpTests {
             Assertions.assertEquals(1001, signUp.getCode());
             Assertions.assertEquals("name: &phone: &address: &email: &password: 7001&re_pass: 7001 &avatar: ", signUp.getMessage());
         }
-        System.out.println("Unit 3: Satisfied");
+        System.out.println("Unit 3: Satisfied!");
     }
 
 }
