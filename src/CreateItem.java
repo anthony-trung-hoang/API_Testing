5b2dde263f742d92926d62c656101fa75ea53131
phuongdd204680 <103870562+phuongdd204680@users.noreply.github.com>
2022-07-08 01:20:09 +0700

    Phuong them nhung ma chay khong ra 1000 OK :<

--- src/CreateItem.java
+++ src/CreateItem.java
@@ -1,3 +1,4 @@
+
 import com.google.gson.Gson;
 
 import java.io.BufferedReader;
@@ -8,19 +9,28 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
+
 public class CreateItem {
     private static HttpURLConnection connection;
 
     public static Rp rp;
+    int rpCode;
+    public String fixed_id;
 
-    public void Test13(String token, String name, int starting_price, int brand_id, String description, String series, String images) {
+    public String fixed_start_date, fixed_end_date;
+
+    public String fixed_title;
+
+
+    public void Test13(String auction_id, String token, String name, String starting_price, String brand_id, String description,String series, String images) {
         String line;
         BufferedReader reader;
         StringBuffer respondContent = new StringBuffer();
 
         // Connect and parse Json
+        /// api/auctions/edit/{auctionId}
         try {
-            URL url = new URL("https://auctions-app-2.herokuapp.com/api/items/create/1");
+            URL url = new URL("https://auctions-app-2.herokuapp.com/api/items/create/" + auction_id);
             connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("POST");
             connection.setRequestProperty("Authorization", "Bearer" + token);
@@ -28,12 +38,11 @@ public class CreateItem {
             connection.setRequestProperty("Content-Type", "application/json");
             connection.setDoOutput(true);
 
-            String data = "{\n \"token\": \"" + token
-                    + "\",\n  \"name\": \"" + name
-                    + "\",\n  \"starting_price\": \"" + starting_price
+            String data = "{\n \"auction_id\": \"" + auction_id
                     + "\",\n  \"brand_id\": \"" + brand_id
-                    + "\",\n  \"description\": \"" + description
                     + "\",\n  \"series\": \"" + series
+                    + "\",\n  \"name\": \"" + name
+                    + "\",\n  \"description\": \"" + description
                     + "\",\n  \"images\": \"" + images
                     + "\"\n}";
             byte[] out = data.getBytes(StandardCharsets.UTF_8);
@@ -43,6 +52,15 @@ public class CreateItem {
             while ((line = reader.readLine()) != null) {
                 respondContent.append(line);
             }
+            System.out.println(respondContent);
+
+            // set to model
+            this.setFixed_id(auction_id);
+            this.setFixed_id(brand_id);
+            this.setFixed_id(series);
+            this.setFixed_id(name);
+            this.setFixed_id(description);
+            this.setFixed_id(images);
 
             // Parse JSON
             Gson g = new Gson();
@@ -56,22 +74,50 @@ public class CreateItem {
         }
 
     }
-
-    public String getToken(){
-        return rp.data.access_token;
-    }
-
     public int getCode() {
         return rp.code;
+    }
+    public int getHttpCode(){
+        return rpCode;
     }
 
     public String getMessage() {
         return rp.message;
     }
 
-    // Trung them
     public Data getData() {
         return rp.data;
     }
 
+    public String getFixed_title() {
+        return fixed_title;
+    }
+
+    public void setFixed_title(String fixed_title) {
+        this.fixed_title = fixed_title;
+    }
+
+    public String getFixed_start_date() {
+        return fixed_start_date;
+    }
+
+    public void setFixed_start_date(String fixed_start_date) {
+        this.fixed_start_date = fixed_start_date;
+    }
+
+    public String getFixed_end_date() {
+        return fixed_end_date;
+    }
+
+    public void setFixed_end_date(String fixed_end_date) {
+        this.fixed_end_date = fixed_end_date;
+    }
+
+    public String getFixed_id() {
+        return fixed_id;
+    }
+
+    public void setFixed_id(String fixed_id) {
+        this.fixed_id = fixed_id;
+    }
 }
---
+++ src/CreateItemTests.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import random.RandomEmail;
+public class CreateItemTests {
+   CreateItem createItem = new CreateItem();
+    Random rand = new Random();
+    RandomEmail email = new RandomEmail();
+    Login login = new Login();
+    Logout logout = new Logout();
+
+    String token ="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9hdWN0aW9ucy1hcHAtMi5oZXJva3VhcHAuY29tXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjU3MDg3NTgyLCJleHAiOjE2NTc0NDc1ODIsIm5iZiI6MTY1NzA4NzU4MiwianRpIjoiUDJ2dWQ2MVV0d3V4TjAwaiIsInN1YiI6MzAzLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.AfGhz_ltry1zkdLJMvUFqY1MHZufibsyY6FHBk--e3M";
+    @Test
+    public void unitTest1(){
+        System.out.println("If ??????, code should be 9995 and message should be Không thể thêm item mới với phiên đấu giá này");
+        System.out.println("Testing unit1...");
+
+        String randomMail = email.getStringWithFixedLength(10) + "@gmail.com";
+        String randomName = email.getSaltString();
+
+        createItem.Test13("1", token, randomName, "10", "1", "jkfsdhjfb", null, null);
+
+        Assertions.assertEquals(9995, createItem.getCode());
+        Assertions.assertEquals("Không thể thêm item mới với phiên đấu giá này", createItem.getMessage());
+        System.out.println("Unit 1: Satisfied!");
+    }
+}
