5ffd4b4ccc96a6ca60a307e5d5a2686f93a78d28
trinhquan1004 <trinhquan04102002@gmail.com>
2022-07-10 12:24:00 +0700

    Quan them

--- src/CreateAuctionTests.java
+++ src/CreateAuctionTests.java
@@ -32,6 +32,7 @@ public class CreateAuctionTests {
         //auctionId:514
         createAuction.Test11("5", "2022/07/12 11:11", "2023/08/21 11:11", title, login.getToken());
         Assertions.assertEquals(1000, createAuction.getCode());
+        Assertions.assertEquals("OK", createAuction.getMessage());
         System.out.println("Unit 1: Satisfied!");
     }
     @Test//chua dang nhap
@@ -43,6 +44,7 @@ public class CreateAuctionTests {
         System.out.println("Testing unit2...");
         createAuction.Test11("3", "2024/06/20 11:11", "2024/06/21 11:11", "Aaaa", null);
         Assertions.assertEquals(1004, createAuction.getCode());//Server returned HTTP response code: 401 for URL
+        Assertions.assertEquals("Chưa đăng nhập", createAuction.getMessage());
         System.out.println("Unit 2: Satisfied!");//yêu cầu ứng dụng khách chưa được hoàn thành vì nó thiếu thông tin xác thực hợp lệ cho tài nguyên được yêu cầu.
     }
 
--- src/DeleteAuction.java
+++ src/DeleteAuction.java
@@ -1,51 +1,37 @@
 import com.google.gson.Gson;
+
 import freq.BaseURL;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
 
-public class CreateBids {
+public class DeleteAuction {
     private static HttpURLConnection connection;
 
     public static Rp rp;
+    //public String fixed_comment_id;
 
-    public int fixed_price;
-
-    public int fixed_bid_last_id;
-
-
-    public void Test21(int price, int bid_last_id,String accessToken,int auction_id) {
+    public void Test13(String accessToken, String auction_id) {
         String line;
         BufferedReader reader;
         StringBuffer respondContent = new StringBuffer();
-
-        // Connect and parse Json
-        /// api/auctions/edit/{auctionId}
         try {
-            URL url = new URL(BaseURL.baseURL + "bids/create/" + auction_id);//auctionId dang dien ra 231
+            URL url = new URL(BaseURL.baseURL + "auctions/deleteAuction/" + auction_id );
             connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("POST");
             connection.setRequestProperty("Authorization", "Bearer" + accessToken);
             connection.setRequestProperty("Accept", "application/json");
             connection.setRequestProperty("Content-Type", "application/json");
             connection.setDoOutput(true);
-            String data = "{\n \"price\": \"" + price + "\"" + "   ,\n  \"bid_last_id\": \"" + bid_last_id  + "\"" + "\n}";
-            byte[] out = data.getBytes(StandardCharsets.UTF_8);
-            OutputStream stream = connection.getOutputStream();
-            stream.write(out);
+
             reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             while ((line = reader.readLine()) != null) {
                 respondContent.append(line);
             }
-
-            // set to model
-            this.setFixed_price(price);
-            this.setFixed_bid_last_id(bid_last_id);
+            System.out.println(respondContent);
 
             // Parse JSON
             Gson g = new Gson();
@@ -59,27 +45,16 @@ public class CreateBids {
         }
 
     }
+
     public int getCode() {
         return rp.code;
     }
 
-    public String getMessage() {
-        return rp.message;
-    }
+    public String getMessage() {return rp.message; }
 
     public Data getData() {
         return rp.data;
     }
 
-
-    public int getFixed_price() { return fixed_price; }
-    public void setFixed_price(int fixed_price) {
-        this.fixed_price = fixed_price;
-    }
-
-    public int getFixed_bid_last_id() { return fixed_bid_last_id; }
-    public void setFixed_bid_last_id(int fixed_bid_last_id) {
-        this.fixed_price = fixed_bid_last_id;
-    }
 
 }
