c17de5a3bfe97f2eb362b160a2feb8fc2449dd52
phuongdd204680 <103870562+phuongdd204680@users.noreply.github.com>
2022-07-11 21:33:07 +0700

    Phuong done

---
+++ src/GetListRates.java
@@ -0,0 +1,59 @@
+import com.google.gson.Gson;
+
+import freq.BaseURL;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class GetListRates {
+    private static HttpURLConnection connection;
+    public static Rp rp;
+
+    public void Test43(int auctionId, int index, int count, String accessToken) {
+        // accessToken is nullable
+        String line;
+        BufferedReader reader;
+        StringBuffer respondContent = new StringBuffer();
+
+        // Connect and parse Json
+        try {
+            URL url = new URL(BaseURL.baseURL + "rates/" + auctionId + "?index=" + index + "&count=" + count);
+
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Authorization", "Bearer" + accessToken);
+
+            reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            while ((line = reader.readLine()) != null) {
+                respondContent.append(line);
+            }
+            System.out.println(respondContent);
+
+            // Parse JSON
+            Gson g = new Gson();
+            rp = g.fromJson(respondContent.toString(), Rp.class);
+
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            connection.disconnect();
+        }
+
+    }
+
+    public int getCode() {
+        return rp.code;
+    }
+
+    public String getMessage() {
+        return rp.message;
+    }
+
+    public Data getData() {
+        return rp.data;
+    }
+}
